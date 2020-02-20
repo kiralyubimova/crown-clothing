aed0c3b6bf46fc126aacf77b47ad0e05b04fa8b4
kiralyubimova <36597731+kiralyubimova@users.noreply.github.com>
2020-02-20 13:56:09 +0300

    finished menu-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      section: [{
+      sections: [
+      {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         id: 1,
@@ -29,6 +31,7 @@ class Directory extends React.Component {
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
         id: 4,
+        size: 'large',
         linkUrl: 'shop/womens'
       },
       {
@@ -36,16 +39,23 @@ class Directory extends React.Component {
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
         id: 5,
+        size: 'large',
         linkUrl: 'shop/mens'
-      }]
-    }
+      }
+      ]
+    };
   }
 
   render() {
     return (
       <div className='directory-menu'>
-
+        {this.state.sections.map(({id, title, imageUrl, size}) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          ))
+        }
       </div>
-    )
+    );
   }
 }
+
+export default Directory;
