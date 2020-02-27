0cc8684d0c33b90221659f45deff01d29c1b6397
kiralyubimova <36597731+kiralyubimova@users.noreply.github.com>
2020-02-27 16:54:07 +0300

    27-02

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,7 +50,13 @@ class Directory extends React.Component {
     return (
       <div className='directory-menu'>
         {this.state.sections.map(({id, title, imageUrl, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            <MenuItem
+              key={id}
+              title={title}
+              imageUrl={imageUrl}
+              size={size}
+              history={this.props.history}
+             />
           ))
         }
       </div>
